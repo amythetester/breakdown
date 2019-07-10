baeb042cc9403bd5e6a202373495a61ed189df12
Amy Wyatt <amy.wyatt12@outlook.com>
2019-07-09 20:48:00 -0700

    added welcome component and router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Ring from './components/ring.js'
+import Welcome from './components/welcome.js'
 
 function App() {
   return (
-    <div className="App fade-in">
-        <Ring />
+    <div>
+      <Router>
+        
+        <Route
+          exact path="/"
+          render={() => <div><nav className="nav navbar navbar-dark bg-dark">
+          <Link to="/" className="navbar-brand">Breakdown</Link>
+          <Link to="/ring"><button className="btn btn-dark nav-item nav-link" type="submit">Begin</button></Link>
+        </nav> <Welcome /></div>}
+        />
+        <Route
+          path="/ring"
+          render={() => <Ring className="App fade-in"/>}
+        />
+      </Router>
     </div>
   );
 }
