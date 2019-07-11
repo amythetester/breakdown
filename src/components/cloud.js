4f71e250a0966695af891ea10a592dfc8193333f
Amy Wyatt <amy.wyatt12@outlook.com>
2019-07-10 18:15:29 -0700

    updated cloud files

---
+++ src/components/cloud.js
@@ -0,0 +1,16 @@
+import './cloud.css';
+import React, {Component} from 'react';
+
+class Cloud extends Component {
+    render() {
+        return (
+            <section>
+                <h1 className="question">
+                    Are there any words you wish to remove?
+                </h1>
+            </section>
+        );
+    }
+};
+
+export default Cloud;
