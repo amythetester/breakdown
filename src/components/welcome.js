173852bc03284aeb57f37866f1da95f3b185a4b7
Amy Wyatt <amy.wyatt12@outlook.com>
2019-07-12 12:25:43 -0700

    timed based transition from ring to question

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -7,7 +7,7 @@ function Welcome() {
       <section className="col-xs-1 text-center">
             <h1>Welcome to Breakdown</h1>
             <p>Click begin to start reducing stress.</p>
-            <Link to="/breathe"><button className="btn btn-info" type="submit" id="start-button">Begin</button></Link>
+            <Link to="/breathe-ring"><button className="btn btn-info" type="submit" id="start-button">Begin</button></Link>
       </section>
     );
   }
