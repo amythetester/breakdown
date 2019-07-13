a7d8d84ba23f92c3312d1abb81ffa509835a9ffc
Amy Wyatt <amy.wyatt12@outlook.com>
2019-07-12 18:32:15 -0700

    added content to landing page

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -4,11 +4,29 @@ import { Link } from "react-router-dom";
 
 function Welcome() {
     return (
-      <section className="col-xs-1 text-center">
+      <div>
+        <section className="col-xs-1 text-center">
             <h1>Welcome to Breakdown</h1>
             <p>Click begin to start reducing stress.</p>
             <Link to="/breathe-ring"><button className="btn btn-info" type="submit" id="start-button">Begin</button></Link>
-      </section>
+        </section>
+        <section className="col-xs-1 text-center">
+          <h2>About Breakdown</h2>
+          <p>
+            Breakdown is an app focused on settling your physical being before diving into your mental state of being. 
+            Using breathing exercises and thoughtful questions, Breakdown helps you break your stress down into manageable 
+            pieces and helps you visualize a better tomorrow.
+          </p>
+        </section>
+        <section className="col-xs-1 text-center">
+          <h2>How does it work?</h2>
+          <p>
+            Modern living has caused undo stress in folks lives. We don't breathe as deeply. We don't stop to settle ourselves. 
+            Breakdown starts the user off with a guided breathing exercise to calm your body and walks through a thought journey
+            to help reduce stress. Press Begin to start.
+          </p>
+        </section>
+      </div>
     );
   }
 
