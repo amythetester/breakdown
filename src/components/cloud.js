0c3188f120b313aac4a2b2f8286a0a3797c8fa69
Amy Wyatt <amy.wyatt12@outlook.com>
2019-07-19 13:45:30 -0700

    fade cloud component in

--- src/components/cloud.js
+++ src/components/cloud.js
@@ -55,7 +55,7 @@ class Cloud extends Component {
         if (this.state.redirect) return <Redirect to={this.props.redirectTo} />;
         if (this.state.removedWords.length > 4) return <Redirect to={this.props.redirectTo} />;
         else return (
-            <div className="center">
+            <div className="center cloudFadeIn">
                 <h1 className="question">
                     {this.props.question}
                 </h1>
