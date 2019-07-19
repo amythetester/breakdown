5ce97faed7a92728241bea3ed3e5d0c79af07795
Amy Wyatt <amy.wyatt12@outlook.com>
2019-07-18 23:52:09 -0700

    added comment to test build

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Question from './components/question.js'
 import Cloud from './components/cloud.js'
 import Finish from './components/finish.js'
 
+// adding a comment
 class App extends Component {
   constructor(props) {
     super(props);
