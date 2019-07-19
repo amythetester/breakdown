9871fb546d10ea4fa6b4001c6737e9eff3ed1eb2
Amy Wyatt <amy.wyatt12@outlook.com>
2019-07-19 12:47:24 -0700

    make begin button easier to see

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -8,10 +8,10 @@ function Welcome() {
         <section className="home">
             <h1>Welcome to Breakdown</h1>
             <h3>Click begin to start reducing stress.</h3>
-            <Link to="/breathe-ring"><button className="btn btn-info" type="submit" id="start-button">Begin</button></Link>
+            <Link to="/breathe-ring"><button className="btn btn-info" type="submit" id="begin-button">Begin</button></Link>
         </section>
         <section className="home">
-          <h2>About Breakdown</h2>
+          <h3>About Breakdown</h3>
           <p>
             Breakdown is an app focused on settling your physical being before diving into your mental state of being. 
             Using breathing exercises and thoughtful questions, Breakdown helps you break your stress down into manageable 
@@ -19,7 +19,7 @@ function Welcome() {
           </p>
         </section>
         <section className="home last-item">
-          <h2>How does it work?</h2>
+          <h3>How does it work?</h3>
           <p>
             Modern living has caused undue stress in folks lives. We don't breathe as deeply. We don't stop to settle ourselves. 
             Breakdown starts the user off with a guided breathing exercise to calm your body and walks through a thought journey
