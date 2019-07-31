3560229ca8ee926430c761f28bc8921af88150f1
Amy Wyatt <amy.wyatt12@outlook.com>
2019-07-30 17:15:22 -0700

    widened mobile ring

--- src/components/action.js
+++ src/components/action.js
@@ -37,15 +37,15 @@ class Action extends Component {
                 </section>
                 <section className="actions">
                     <section className="action">
-                        <h3>Have 5 minutes?</h3>
+                        <h3>Have 1 minute?</h3>
                         <p>{this.props.actions[0]}</p>
                     </section>
                     <section className="action">
-                        <h3>Have 10 minutes?</h3>
+                        <h3>Have 5 minutes?</h3>
                         <p>{this.props.actions[1]}</p>
                     </section>
                     <section className="action">
-                        <h3>Have more time?</h3>
+                        <h3>Have 10 minutes?</h3>
                         <p>{this.props.actions[2]}</p>
                     </section>
                 </section>
@@ -59,7 +59,7 @@ Action.defaultProps = {
     actions: [ 
         "Drink some water or have a snack. Remember we are like complex plants with emotions.", 
         "Take a moment to do some quick stretches. Lower your shoulders from your ears. Relax your jaw. Stand if you are able. Roll your neck a couple of times each direction.",
-        "Go for a walk, jog, or just enjoy time outside. Sometimes we just need a change of scenary to help clear our thoughts. Take in that fresh air and enjoy your time away.",
+        "Go for a walk, jog, or just enjoy time outside. Sometimes we just need a change of scenary to help clear our thoughts. Take in that fresh air, you deserve it.",
     ]
 };
 
